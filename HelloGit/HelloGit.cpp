7a88cf2798968a6b54573bd660a4e1b3f2a4e95d
ttotti <prs12344@naver.com>
2018-09-29 23:25:46 +0900

    second commit

--- HelloGit/HelloGit.cpp
+++ HelloGit/HelloGit.cpp
@@ -6,3 +6,18 @@ int main()
 
 	return 0;
 }
+
+// ±ê Çãºê ±âº» Ä¿¹Ô ÇÏ´Â ¹ý
+/*
+	echo "# HelloGitHub" >> README.md
+
+	git init
+
+	git add *
+
+	git commit - m "Ä¿¹Ô ÀÌ¸§"
+
+	git remote add origin ±ê Çãºê HTTPS
+
+	git push - u origin master
+*/
